098c2a2177e621d983bf1c0a84bade3782f0f22f
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2023-05-07 13:38:59 -0400

    updated

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,18 +1,17 @@
 class HooksController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:ipn_webhook]
+    skip_before_action :verify_authenticity_token, only: [:ipn_webhook]    
 
+    # before_action :verify_webhook
 
-    
 
     def ipn_webhook
 
-      payload1 = request.body
+      payload = request.body.read
+      signature = request.headers["X-Hubspot-Signature"]
+        
+      pp payload
 
-      payload1 = request
-
-      puts payload1
-
-      puts payload1
+      pp signature
 
      
 
@@ -42,25 +41,20 @@ class HooksController < ApplicationController
       #     event.save!
 
       # end
-      
 
 
-   
-    
-      
-     # If the body contains the survey_name parameter...
-    #  if params[:survery_name].present?
-        # Create a new Survey object based on the received parameters...
-        # survey = Survey.new(:name => params[:survey_name]
-        # survey.url = params[:survey_url]
-        # survey.creator_email = params[:survey_creator_email]
-        # survey.save!
-    #   end
+      private
+
+      # def verify_webhook
+      #   secret = HubspotEvent.client_secret
+      #   payload = request.body.read
+      #   signature = request.headers["X-Hubspot-Signature"]
+      #   computed = Digest::SHA256.hexdigest(secret + payload)
   
-      # The webhook doesn't require a response but let's make sure
-      # we don't send anything
-      # render :nothing => true
-    end
+      #   raise SignatureVerificationError unless signature == computed
+      # end
+      
+
 
     
 end
