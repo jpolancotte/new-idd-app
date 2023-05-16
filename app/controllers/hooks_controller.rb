c4d8f6c8d388fbc5578ae9be295fa9fcc2eced6f
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2023-05-16 19:40:54 -0400

    updated

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -45,8 +45,8 @@ class HooksController < ApplicationController
           puts wh['propertyValue']
 
           if wh['propertyName'] == "dealstage"
-           deal=DealStage.find_by_number(wh['propertyValue'])
-           deal=Deal.update(deal.id, {"name" => wh['propertyValue']} )  
+           deal_stage_name=DealStage.find_by_number(wh['propertyValue']).name
+           deal=Deal.update(deal.id, {"deal_stage_id" => deal_stage_name} )  
           else
             deal=Deal.update(deal.id, {wh['propertyName'] => wh['propertyValue']} )
           end         
