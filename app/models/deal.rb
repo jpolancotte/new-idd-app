f1fa7ba87998d0fd4667f5ecdfb8af3198c86d6a
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2023-05-16 18:41:26 -0400

    updated

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,5 +2,10 @@ class Deal < ApplicationRecord
   belongs_to :team, optional: true
   belongs_to :deal_stage, optional: true
 
-  validates :name, :presence => true, :uniqueness => true
+  validates :dealname, :presence => true, :uniqueness => true
+
+  # def name
+  #   dealname
+  # end
+
 end
