5dd46366077055ece791d86b45bc937f193a89f0
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2023-05-16 18:46:39 -0400

    updated

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -63,6 +63,10 @@ class HooksController < ApplicationController
 
           lead_name = deal.dealname
 
+          poc = deal.probability_of_close.to_i
+
+          poc="#{0.poc}"
+
           a="eyJ0eXAiOiJKV1QiLCJub25jZSI6IkRWcVdKYXlwRVd6V3BfNmItVmhqZ1NGRWhKZnAwdEx5ZHNBdGw1akFQYnciLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC81YzQ2NDlmOC04MGM2LTRhMTItODEwNy01ZGQ3ZTZlY2QyNzcvIiwiaWF0IjoxNjg0Mjc0MTQ2LCJuYmYiOjE2ODQyNzQxNDYsImV4cCI6MTY4NDM2MDg0NiwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFZUUFlLzhUQUFBQTF1dnlEN1VOWWtjMktLSkFSVGtuTGw5RVFlZHMvcWtrcXl2eTBjWVF4akNScHplc0ViOHRpb21LZkRuN3BqK1FHRVVTVVl4QWlWMXRsUFp2c05ZR2pINzdlQnVSVEVlZGhIY09pT3NOdmJvQjlwNjhQN0JqazlJTHFQVS9xM1BxTDFZb1VHWWQ2eWc1aHdiMEdnVFpVMWlRK0MzRTdBL0NkNE1XUVNKRlE0MD0iLCJhbXIiOlsicnNhIiwibWZhIl0sImFwcF9kaXNwbGF5bmFtZSI6IkdyYXBoIEV4cGxvcmVyIiwiYXBwaWQiOiJkZThiYzhiNS1kOWY5LTQ4YjEtYThhZC1iNzQ4ZGE3MjUwNjQiLCJhcHBpZGFjciI6IjAiLCJjYXBvbGlkc19sYXRlYmluZCI6WyJhNTZkN2JjMC01ODQwLTQ0N2EtODJhNS1jM2VhYjA2YjdkMjgiXSwiZGV2aWNlaWQiOiJjMTk3ZDcxMS03OTkxLTQwMjMtYmVhZC1iMjhjMmE1YTBjMzIiLCJmYW1pbHlfbmFtZSI6IlBvbGFuY28iLCJnaXZlbl9uYW1lIjoiSm9zZSIsImlkdHlwIjoidXNlciIsImlwYWRkciI6IjY5LjExNy4yMDguMTkzIiwibmFtZSI6Ikpvc2UgUG9sYW5jbyIsIm9pZCI6IjllNWIzMzlmLWI0ZmMtNDIzMi1hZDM3LWI0M2FhNGE4YWE2ZiIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS02NTAwODIxNDEtMTkyMTQwMDk3NS0zMzAwODc5MjA5LTM3MTEiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzIwMDFBNDNEN0ZCMyIsInJoIjoiMC5BVkVBLUVsR1hNYUFFa3FCQjEzWDV1elNkd01BQUFBQUFBQUF3QUFBQUFBQUFBQlJBTVkuIiwic2NwIjoiQXVkaXRMb2cuUmVhZC5BbGwgRGV2aWNlTWFuYWdlbWVudE1hbmFnZWREZXZpY2VzLlJlYWQuQWxsIERldmljZU1hbmFnZW1lbnRNYW5hZ2VkRGV2aWNlcy5SZWFkV3JpdGUuQWxsIERpcmVjdG9yeS5SZWFkLkFsbCBGaWxlcy5SZWFkIEZpbGVzLlJlYWQuQWxsIEZpbGVzLlJlYWRXcml0ZSBGaWxlcy5SZWFkV3JpdGUuQWxsIG9wZW5pZCBwcm9maWxlIFNpdGVzLk1hbmFnZS5BbGwgU2l0ZXMuUmVhZC5BbGwgU2l0ZXMuUmVhZFdyaXRlLkFsbCBVc2VyLlJlYWQgZW1haWwiLCJzaWduaW5fc3RhdGUiOlsiZHZjX21uZ2QiLCJkdmNfY21wIiwia21zaSJdLCJzdWIiOiJkY0M1aklqOEY1QV9rREltUVRGSldZdWZFVXpaY1J6bGVnOUJfMU1ENkhjIiwidGVuYW50X3JlZ2lvbl9zY29wZSI6Ik5BIiwidGlkIjoiNWM0NjQ5ZjgtODBjNi00YTEyLTgxMDctNWRkN2U2ZWNkMjc3IiwidW5pcXVlX25hbWUiOiJqb3NlLnBvbGFuY29AdGFycnl0b3duZXhwb2NhcmUuY29tIiwidXBuIjoiam9zZS5wb2xhbmNvQHRhcnJ5dG93bmV4cG9jYXJlLmNvbSIsInV0aSI6IkR6a0xFQXFlcWtpdnpsZXg5VE1QQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdLCJ4bXNfY2MiOlsiQ1AxIl0sInhtc19zc20iOiIxIiwieG1zX3N0Ijp7InN1YiI6Ijd2TDRWTVpHNGhmLUdUQURXMUgxdDR3TzVudXk4VkwxWXJhakJNcEZJSkUifSwieG1zX3RjZHQiOjE1ODM5MzM1MDV9.IX4ZaWLoBU6nXRmowG57o5pG8w1bQVkUE-hLJIGLaFMRyaMjcjoEv-snqOuefuzDXNE14uIiNo5tV6ah7ZoGhTm_W9uwGvoFGIT6d-PltLO7wmnRov4pfSwmytA2nnrLKFQOXDTKcJnP_kJJqE6QTWyQngHlh_aU94z9p1AzxjLMzqU3V7sAuIWZjI_HwYuBiWJCGHqCN7FXRk8yXbVE7w4_laRFrY5UIyfuErJJ5eLFARPZAXz76qosbx1lZscANvhX6_50ni00DW3cIJWBpXnyI3MXTZXlrCgmZxt7HIOG3J0A5dXuFAtoKbbl4VzIeYBBNy01gPnQ3rOca7zg2Q"
 
           get_str = "https://graph.microsoft.com/v1.0/me/drive/root:/JP Pipleline TEST.xlsx:/workbook/worksheets/Master Data/tables/table1/rows" #add
@@ -94,7 +98,7 @@ class HooksController < ApplicationController
                   "values"=>[
                     [deal.team.name, lead_name, deal.chain, 
                     deal.deal_stage, deal.state, deal.tte_servicing_pharmacy, deal.total_residential_individuals, 
-                    0.deal.probability_of_close.to_i, 44166,
+                    poc, 44166,
                     deal.incumbent_pharmacy, deal.delivery_type, deal.comments, deal.pipeline_date, deal.forecasted_individuals,
                     "=IFERROR(EDATE(I#{total},-1),"")", 
                     "=EDATE(I#{total},1)",
