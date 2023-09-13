d87c4db9ff61b14b52f632437b67c86a132853e7
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2023-09-13 13:00:34 -0400

    updated

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,7 +1,8 @@
 class State < ApplicationRecord
     validates :name, :presence => true, :uniqueness => true
     has_many :npi_addresses, dependent: :destroy
-    has_many :npi_companies, dependent: :destroy
+    has_many :npi_companies, dependent: :destroy   
+    has_many :parents, through: :npi_companies
     
     scope :active, -> { where(active: true) }
     scope :inactive, -> { where(active: false) }
