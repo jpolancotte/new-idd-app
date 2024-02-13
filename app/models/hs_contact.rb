09fd0071b9fda9a80bf534428a0ad084a134e601
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2024-02-13 14:17:52 -0500

    updated

--- app/models/hs_contact.rb
+++ app/models/hs_contact.rb
@@ -1,13 +1,13 @@
 class HsContact < ApplicationRecord
     validates :hs_number, presence: true, uniqueness: true
 
-    geocoded_by :get_address
-    after_validation :geocode, if: ->(obj){ obj.contact_city.present? and obj.contact_city_changed? }
+    # geocoded_by :get_address
+    # after_validation :geocode, if: ->(obj){ obj.contact_city.present? and obj.contact_city_changed? }
     
-    #after_validation :geocode, if: ->(obj){ obj.latitude.nil? }
+    # #after_validation :geocode, if: ->(obj){ obj.latitude.nil? }
     
-    def get_address
-    "#{contact_city}, #{contact_state}"
-    end
+    # def get_address
+    # "#{contact_city}, #{contact_state}"
+    # end
 
 end
