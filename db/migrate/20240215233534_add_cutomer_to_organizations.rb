bc9429009d79d0312f2298b5ef436a829080a9b0
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2024-02-15 18:57:05 -0500

    added customer:booleant to Org

---
+++ db/migrate/20240215233534_add_cutomer_to_organizations.rb
@@ -0,0 +1,5 @@
+class AddCutomerToOrganizations < ActiveRecord::Migration[7.0]
+  def change
+    add_column :organizations, :customer, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_15_225847) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_15_233534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -212,6 +212,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_15_225847) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "customer"
   end
 
   create_table "parents", force: :cascade do |t|
