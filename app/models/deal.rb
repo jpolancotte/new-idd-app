03e7c94ab39286f8b21095cc6f092d5827533841
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2025-02-06 12:26:07 -0500

    updated

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,6 +1,7 @@
 class Deal < ApplicationRecord
   # validates :dealname, :presence => true, :uniqueness => true
-  validates :objectid, :presence => true, :uniqueness => true
+  validates :objectid, presence: true, db_uniqueness: true
+  
   belongs_to :team, optional: true
   belongs_to :deal_stage, optional: true 
   has_many :events, dependent: :destroy
