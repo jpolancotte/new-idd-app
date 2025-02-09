51138a04929c3719491dbf392044e7f46a91f365
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2025-02-09 13:17:50 -0500

    updated

--- app/models/deal_pipeline_activity.rb
+++ app/models/deal_pipeline_activity.rb
@@ -1,4 +1,6 @@
 class DealPipelineActivity < ApplicationRecord
   belongs_to :deal
   belongs_to :pipeline_activity
+
+  validates :deal_id, uniqueness: {scope: :pipeline_activity_id}
 end
--- app/models/pipeline_activity.rb
+++ app/models/pipeline_activity.rb
@@ -2,7 +2,9 @@ class PipelineActivity < ApplicationRecord
   has_many :events, dependent: :destroy
 
   has_many :deal_pipeline_activities, dependent: :destroy
-  has_many :deals, -> { distinct }, through: :deal_pipeline_activities
+  # has_many :deals, -> { distinct }, through: :deal_pipeline_activities
+
+  has_many :deals, through: :deal_pipeline_activities
    
   def name
     end_date
