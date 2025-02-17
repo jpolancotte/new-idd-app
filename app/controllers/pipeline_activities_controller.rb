0af435d1d4b26f174512ed4f7a0bf25d99137c3c
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2025-02-17 01:12:30 -0500

    updated

--- app/controllers/pipeline_activities_controller.rb
+++ app/controllers/pipeline_activities_controller.rb
@@ -12,9 +12,7 @@ class PipelineActivitiesController < ApplicationController
   # GET /pipeline_activities/1 or /pipeline_activities/1.json
   def show
     if params[:team_id] 
-
       @deals = @pipeline_activity.deals.where(team_id: params[:team_id]  ) 
-
     else
       @deals = @pipeline_activity.deals 
     end
