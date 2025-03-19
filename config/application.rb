bde8d8e8b530819d39e4af63929a4de2187442d5
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2025-03-19 13:44:02 -0400

    updated

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module NewIddApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.time_zone = 'Eastern Time (US & Canada)'
+    # config.time_zone = 'Eastern Time (US & Canada)'
 
     # Configuration for the application, engines, and railties goes here.
     #
