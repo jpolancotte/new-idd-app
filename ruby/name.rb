9d4f80f20d9b1ea94bf846aa617a9774c9df8cf0
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2025-05-22 17:40:18 -0400

    updated

--- ruby/name.rb
+++ ruby/name.rb
@@ -7,8 +7,8 @@ require 'csv'
 require 'pp'
 # require 'mechanize'
 
-CSV.open("csv/410_cust_results.csv", "a") do |fl|
-  CSV.foreach("csv/410_cust.csv", headers: true, :encoding => 'windows-1251:utf-8', header_converters: :symbol, converters: :all) do |row|     
+CSV.open("csv/may_13_25_results.csv", "a") do |fl|
+  CSV.foreach("csv/may_13_25.csv", headers: true, :encoding => 'windows-1251:utf-8', header_converters: :symbol, converters: :all) do |row|     
     numb=row[0]
     numb=numb.to_s
     first=row[1]
@@ -24,10 +24,10 @@ CSV.open("csv/410_cust_results.csv", "a") do |fl|
     
     full_name = "#{first} #{last}"
     
-    file_name = "#{numb}-#{first}-#{last}-April-10-2025-Customer-Webinar-CEU-Certificate"     
+    file_name = "#{numb}-#{first}-#{last}-May-13-2025-Virtual-Symposium-CEU-Certificate"     
     
 
-    url="https://20711908.fs1.hubspotusercontent-na1.net/hubfs/20711908/2025%20Webinars/April%2010%202025/Customer/#{file_name}.pdf"
+       url="https://20711908.fs1.hubspotusercontent-na1.net/hubfs/20711908/2025%20Symposiums/May%2013%202025%20PA/Virtual/#{file_name}.pdf"
   
     fl << row.values_at(*row.headers) + [first] + [last] + [full_name] + [file_name] + [url]
 
