82bb8720d05b6fb6b9b1484ef50a89dfedacac69
Jose Polanco <jose.polanco@tarrytownexpocare.com>
2025-11-18 11:47:41 -0500

    updated

--- ruby/name.rb
+++ ruby/name.rb
@@ -7,8 +7,8 @@ require 'csv'
 require 'pp'
 # require 'mechanize'
 
-CSV.open("csv/oct9_cus_results.csv", "a") do |fl|
-  CSV.foreach("csv/oct9_cus.csv", headers: true, :encoding => 'windows-1251:utf-8', header_converters: :symbol, converters: :all) do |row|     
+CSV.open("csv/11_13_25_web_results.csv", "a") do |fl|
+  CSV.foreach("csv/11_13_25_web.csv", headers: true, :encoding => 'windows-1251:utf-8', header_converters: :symbol, converters: :all) do |row|     
     numb=row[0]
     numb=numb.to_s
     first=row[1]
@@ -24,7 +24,7 @@ CSV.open("csv/oct9_cus_results.csv", "a") do |fl|
     
     full_name = "#{first} #{last}"
     
-    file_name = "#{numb}-#{first}-#{last}-October-9-2025-Customer-Webinar-CEU-Certificate"    
+    file_name = "#{numb}-#{first}-#{last}-November-13-2025-Customer-Webinar-CEU-Certificate"    
 
   
     url="https://20711908.fs1.hubspotusercontent-na1.net/hubfs/20711908/2025%20Webinars/October%209%202025/Certificates/#{file_name}.pdf"
